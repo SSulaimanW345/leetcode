2f92bf838a0cdcb9521869940355366622be4f13
SSulaimanW345 <s.s.waleed.e60@gmail.com>
2025-02-28 00:00:18 +0400

    solved 2 problems

---
+++ ArrayProblems/ContainsDuplicate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.ArrayProblems
+{
+    public class ContainsDuplicate
+    {
+        // O(N) Space // O(N) time
+        public static bool ContainsDuplicateUseSet(int[] arr)
+        {
+            HashSet<int> numberSet = new HashSet<int>(); 
+            foreach (int num in arr) 
+            {
+                if (numberSet.Contains(num)) return true;
+                numberSet.Add(num);
+            }
+            return false;
+        }
+        //O(n log n) Time, O(1) Space
+        public static bool ContainsDuplicateUseSorting(int[] arr)
+        {
+            Array.Sort(arr);
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i - 1] == arr[i]) return true;
+            }
+            return false;
+        }
+        //O(n) Time, O(n) Space
+        public static bool ContainsDuplicateUseDictionary(int[] arr)
+        {
+            Dictionary<int, bool> numDict = new Dictionary<int, bool>();
+            foreach (int num in arr)
+            {
+                if (numDict.ContainsKey(num)) return true; numDict.Add(num, true);
+            }
+            return false;
+        }
+
+    }
+}
---
+++ ArrayProblems/ContainsDuplicate11.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.ArrayProblems
+{
+    public class ContainsDuplicate11
+    {
+        public static bool ContainsDuplicate11UseDict(int[] arr, int k) 
+        {   
+            Dictionary<int,int> numsDict = new Dictionary<int,int>();
+            for (int i=0;i< arr.Length;i++)
+            {
+                if (numsDict.ContainsKey(arr[i])) 
+                {
+                    if (Math.Abs(numsDict[arr[i]] - i) <= k) return true;
+                    numsDict[arr[i]] = i;
+                }
+                else numsDict[arr[i]] = i;
+            }
+            return false;
+        }
+    }
+}
